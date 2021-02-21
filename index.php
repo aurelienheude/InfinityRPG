b833b25f146b340b01f86752156d501077e4bfe3
aurelienheude <aurelienheudepro@gmail.com>
2021-02-21 14:39:39 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infinity RPG</title>
 
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav>
+        <h1>Infinity RPG</h1>
+    </nav>
+
+    <div class="inventory">
+        <h2>Inventaire</h2>
+
+        <ul>
+            <li>Epée x46</li>
+            <li>corne de démon x31</li>
+            <li>bouclier de doran</li>
+            <li>carapace brisée</li>
+            <li>peau de gloot x12</li>
+        </ul>
+    </div>
+    <div class="Profil">
+        <h2>Profil</h2>
+        <ul>
+            <li> Niveau 43</li>
+            <li>force</li>
+            <li>agilité</li>
+            <li>vitesse</li>
+            <li>défense</li>
+        </ul>
+    </div>
+</body>
+</html>
